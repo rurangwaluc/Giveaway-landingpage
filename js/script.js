bd0ac7ded23186c47d8bff75fd858f6ed87ae1ba
rurangwaluc <rurangwajl7@gmail.com>
2021-09-13 09:24:14 +0200

    update commit

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
         hour = minute * 60,
         day = hour * 24;
 
-    let new_year = "Dec 1, 2021 00:00:00",
+    let new_year = "Oct 1, 2021 00:00:00",
         countDown = new Date(new_year).getTime(),
         x = setInterval(function () {
 
